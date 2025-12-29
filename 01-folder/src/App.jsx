7295f03f3c0a0850d598d75b308dcd7e3ccfaa38
CodeWithDevansh7 <devanshgera518@gmail.com>
2025-12-29 16:31:59 +0530

    folder creation with vite

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // React fragment to avoid extra nodes in the DOM (empty tags)
+    <>  
+      <h1>Hello, World!</h1>
+      <p>Welcome to my React application.</p>    
+    </>
+    
+  )
+}
+
+export default App
