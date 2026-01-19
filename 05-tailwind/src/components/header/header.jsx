f37287f0398fc33ea84bd54b436da97480242ebc
CodeWithDevansh7 <devanshgera518@gmail.com>
2026-01-19 20:31:23 +0530

    used leading property for spacing

--- 05-tailwind/src/components/header/header.jsx
+++ 05-tailwind/src/components/header/header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const header = () => {
   return (
-    <div className='flex justify-between ml-10 mr-23 my-5'>
-      <button className="rounded-full px-3 py-2 cursor-pointer bg-black text-white">Target Audience</button>
-      <button className="rounded-full px-5 py-2 cursor-pointer bg-gray-200">Digital Banking Platform</button>
+    <div className='flex justify-between mx-10 my-5'>
+      <button className="rounded-full px-3 py-2 cursor-pointer bg-black text-white uppercase">Target Audience</button>
+      <button className="rounded-full px-5 py-2 cursor-pointer bg-gray-200 uppercase">Digital Banking Platform</button>
     </div>
   )
 }
--- 05-tailwind/src/components/left-content/content.jsx
+++ 05-tailwind/src/components/left-content/content.jsx
@@ -3,10 +3,10 @@ import {ArrowUpRight} from 'lucide-react'
 
 const content = () => {
   return (
-    <div className='w-[320px] mx-10' >
-      <h1 className='font-semibold text-5xl'>Prospective Customer Segmentation</h1>
-      <p className='my-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odit perspiciatis enim earum asperiores at hic et ipsa officiis nostrum.</p>
-      <button className='absolute bottom-20'><ArrowUpRight size={50}/></button>
+    <div className='w-1/4 mx-10' >
+      <h1 className='font-semibold text-6xl leading-[1.1]'>Prospective Customer Segmentation</h1>
+      <p className='my-10 text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odit perspiciatis enim earum asperiores at hic et ipsa officiis nostrum.</p>
+      <button className='absolute bottom-20'><ArrowUpRight size={80}/></button>
     </div>
   )
 }
